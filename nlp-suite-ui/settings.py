b68db16a257e48d87a4c90a986eb2d4ce43cae10
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-04-04 22:11:25 -0400

    update localhost links

--- nlp-suite-ui/settings.py
+++ nlp-suite-ui/settings.py
@@ -20,58 +20,58 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-42vf+n(=f16c8_upkhwjyst!vi&u_li-f)ru6+o-#k-)!c-_$k'
+SECRET_KEY = "django-insecure-42vf+n(=f16c8_upkhwjyst!vi&u_li-f)ru6+o-#k-)!c-_$k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'ui.apps.UIConfig',
-    'django.contrib.contenttypes',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
+    "ui.apps.UIConfig",
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+    "django.contrib.messages",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
-ROOT_URLCONF = 'nlp-suite-ui.urls'
+ROOT_URLCONF = "nlp-suite-ui.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'ui', 'templates'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "ui", "templates"),
         ],
-        'APP_DIRS': False,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": False,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'nlp-suite-ui.wsgi.application'
+WSGI_APPLICATION = "nlp-suite-ui.wsgi.application"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -81,15 +81,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://172.16.0.11:3000",
+    "http://localhost:3000",
+    "http://0.0.0.0:3000",
+]
--- ui/views.py
+++ ui/views.py
@@ -1,57 +1,76 @@
 import requests
-
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
-from django.template import loader
-from django.shortcuts import render
 from django.contrib import messages
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseRedirect,
+)
+from django.shortcuts import render
+from django.template import loader
+
 
 def index(_: HttpRequest):
-    template = loader.get_template('../templates/index.html')
+    template = loader.get_template("../templates/index.html")
     return HttpResponse(template.render())
+
 
 def home(_: HttpRequest):
-    template = loader.get_template('../templates/home.html')
+    template = loader.get_template("../templates/home.html")
     return HttpResponse(template.render())
+
 
 def install(_: HttpRequest):
-    template = loader.get_template('../templates/install.html')
+    template = loader.get_template("../templates/install.html")
     return HttpResponse(template.render())
+
 
 def contact(_: HttpRequest):
-    template = loader.get_template('../templates/contact.html')
+    template = loader.get_template("../templates/contact.html")
     return HttpResponse(template.render())
 
+
 def status(_: HttpRequest):
-    template = loader.get_template('../templates/status.html')
+    template = loader.get_template("../templates/status.html")
     return HttpResponse(template.render())
+
 
 def sentiment_analysis(request: HttpRequest):
     if request.method == "POST":
         print(request.body)
-        response = requests.post("http://localhost:3000/sentiment_analysis", data=request.body, headers=request.headers)
+        response = requests.post(
+            "http://0.0.0.0:3000/sentiment_analysis",
+            data=request.body,
+            headers=request.headers,
+        )
         if response.ok:
             return HttpResponseRedirect("/status")
         else:
             messages.add_message(request, messages.ERROR, response.content.decode())
             print(response.content.decode())
-    return render(request, 'sentiment_analysis.html')
+    return render(request, "sentiment_analysis.html")
+
 
 def set_up(_: HttpRequest):
-    template = loader.get_template('../templates/set_up.html')
+    template = loader.get_template("../templates/set_up.html")
     return HttpResponse(template.render())
+
 
 def all_options_gui(_: HttpRequest):
-    template = loader.get_template('../templates/all_options_gui.html')
+    template = loader.get_template("../templates/all_options_gui.html")
     return HttpResponse(template.render())
+
 
 def SOV(_: HttpRequest):
-    template = loader.get_template('../templates/SOV.html')
+    template = loader.get_template("../templates/SOV.html")
     return HttpResponse(template.render())
+
 
 def shape_of_stories(_: HttpRequest):
-    template = loader.get_template('../templates/shape_of_stories.html')
+    template = loader.get_template("../templates/shape_of_stories.html")
     return HttpResponse(template.render())
 
+
 def style_analysis(_: HttpRequest):
-    template = loader.get_template('../templates/style_analysis.html')
+    template = loader.get_template("../templates/style_analysis.html")
     return HttpResponse(template.render())
