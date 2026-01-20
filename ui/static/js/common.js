ee213337d8f9070d620288674b599dfe8d334045
epamaya1 <evanamaya1234@gmail.com>
2026-01-20 12:53:09 -0700

    + js array / ui match backend submit

--- ui/static/js/common.js
+++ ui/static/js/common.js
@@ -1,6 +1,6 @@
-function listofitems(){
+function listofitems2(){
 
-    var optionsSelect = document.getElementById("ngrams_options_menu_var");
+    var optionsSelect = document.getElementById("ngrams_options_list");
     var pickedSelect = document.getElementById("options");
     var plusbutton = document.getElementsByName("options-+")[0];
     var resetbutton = document.getElementsByName("options-reset")[0];
@@ -30,4 +30,30 @@ function listofitems(){
     }
 }
 
-listofitems()
+listofitems2()
+
+
+function submitbutton() {
+  var sel1 = document.getElementById("options");
+  var sel2 = document.getElementById("optionss");
+
+  var out1 = "[";
+  for (var i = 0; i < sel1.options.length; i++) {
+    out1 = out1 + '"' + sel1.options[i].value + '"';
+    if (i < sel1.options.length - 1) out1 = out1 + ",";
+  }
+  out1 = out1 + "]";
+
+  var out2 = "[";
+  for (var j = 0; j < sel2.options.length; j++) {
+    out2 = out2 + '"' + sel2.options[j].value + '"';
+    if (j < sel2.options.length - 1) out2 = out2 + ",";
+  }
+  out2 = out2 + "]";
+
+  document.getElementById("ngrams_options_list_hidden").value = out1;
+  document.getElementById("viewer_options_list_hidden").value = out2;
+
+  return true;
+}
+
